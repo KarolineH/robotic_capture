cf90422c64891feef68802b1ae272a599bbcb7f6
KarolineH <karoline@heiwolt.de>
2024-05-21 22:12:00 +0100

    added a rudimentary max freq test

---
+++ max_control_freq_test.py
@@ -0,0 +1,18 @@
+import kinova_arm_if.helpers.kortex_util as k_util
+import kinova_arm_if.helpers.data_io as data_io
+from kinova_arm_if.arm_if import Kinova3
+import time
+
+args = k_util.parseConnectionArguments()
+with k_util.DeviceConnection.createTcpConnection(args) as router:
+    IF = Kinova3(router)
+    success = True
+
+    starting_pose = IF.get_pose() # [x,y,z,theta_x,theta_y,theta_z]
+    pose = starting_pose
+    starting_time = time.time()
+    for i in range(100):
+        pose[2] = pose[2] + 0.0001 # increment the z position by 0.00001 meters
+        action = data_io.create_cartesian_action(pose=pose)
+        success &= IF.execute_action(action)
+        print(time.time() - starting_time)
